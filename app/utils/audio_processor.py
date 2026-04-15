66c25b79a83eafb4f9531698d1508df794316d38
Le-Who <danoner123@protonmail.com>
2026-04-16 00:33:06 +0300

    fix(audio): export transcribe_voice from backwards-compatibility shim to resolve broken test

--- app/utils/audio_processor.py
+++ app/utils/audio_processor.py
@@ -6,4 +6,5 @@ All new code should import directly from ``app.utils.multimodal_processor``.
 
 from app.utils.multimodal_processor import (  # noqa: F401
     THINKING_CONFIG_HIGH as THINKING_CONFIG,
+    transcribe_voice,
 )
